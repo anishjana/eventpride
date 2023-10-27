b723225d52565377617c3ef08c4fa4903c98cdb0
anishjana <52600343+anishjana@users.noreply.github.com>
2023-10-27 15:08:24 +0530

    home page changes

--- src/components/placeholder.js
+++ src/components/placeholder.js
@@ -5,7 +5,7 @@ export default function PlaceHolder(props) {
   const { title, subtitle, image } = props
   return (
     <>
-      <header className={`page-head p pt-10px`}>
+      <header className={`page-head pt-10px`}>
         <Img fluid={image} style={{ width: "100%", height: "auto" }}></Img>
         <span className={"text-black absolute ml-12 flex flex-col items-start"}>
           {title && <h1 className="mb-0">{title}</h1>}
